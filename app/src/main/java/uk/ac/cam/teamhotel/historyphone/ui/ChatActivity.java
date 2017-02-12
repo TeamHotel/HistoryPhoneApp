69e00133398d1fa217040f51a424439256902c1a
Harry Graham <harryg_95@icloud.com>
2017-02-12 19:06:10 +0000

    Add ChatAdapter for conversation screen, ready to implement further. Add two more listitem xml files for send/receive layouts

--- app/src/main/java/uk/ac/cam/teamhotel/historyphone/ui/ChatActivity.java
+++ app/src/main/java/uk/ac/cam/teamhotel/historyphone/ui/ChatActivity.java
@@ -3,22 +3,55 @@ package uk.ac.cam.teamhotel.historyphone.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import uk.ac.cam.teamhotel.historyphone.R;
+import uk.ac.cam.teamhotel.historyphone.database.ChatMessage;
 
 public class ChatActivity extends AppCompatActivity {
 
     private static final String TAG = "ChatActivity";
 
+    public static List<ChatMessage> chatMessageList = new ArrayList<ChatMessage>();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+
+
+        ListView chatMessages = (ListView) findViewById(R.id.chat_list);
+        chatMessages.setAdapter(new ChatAdapter(getApplicationContext(),chatMessageList ));
     }
 
     /**
      * Called when the user taps the send button.
      */
     public void sendMessage(View view) {
-        // TODO: implement.
+        // TODO: add ChatMessage object to DB.
+
+        //get the text field view
+        EditText editText = (EditText) findViewById(R.id.enterText);
+
+        //setup new ChatMessage object to store in db
+        ChatMessage newMessage = new ChatMessage();
+        newMessage.setMessage_text(editText.getText().toString());
+        newMessage.setFrom_user(true);
+
+        //reset the text view to empty
+        editText.setText("");
+
     }
+
+    /**
+     * Called when loading a conversation for the first time.
+     */
+    public void loadChatListFromDB(){
+        //TODO: implement - load messages from DB into ArrayList.
+    }
+
 }
---
+++ app/src/main/java/uk/ac/cam/teamhotel/historyphone/ui/ChatAdapter.java
@@ -0,0 +1,91 @@
+package uk.ac.cam.teamhotel.historyphone.ui;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+
+import uk.ac.cam.teamhotel.historyphone.R;
+import uk.ac.cam.teamhotel.historyphone.artifact.Artifact;
+import uk.ac.cam.teamhotel.historyphone.database.ChatMessage;
+
+public class ChatAdapter extends ArrayAdapter<ChatMessage> {
+
+    public ChatAdapter(Context context, List<ChatMessage> messages) {
+        super(context, R.layout.list_item_message_reply, messages);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View view, @NonNull ViewGroup parent) {
+        // Get the data item for this position.
+        ChatMessage chatMessage = getItem(position);
+
+        //get the type of the layout
+        int type = getItemViewType(position);
+
+        // Check if an existing view is being reused, otherwise inflate the view.
+        if (view == null) {
+            if(type ==0) {
+                view = LayoutInflater.from(getContext()).inflate(R.layout.list_item_message_reply, parent, false);
+            }else{
+                view = LayoutInflater.from(getContext()).inflate(R.layout.list_item_message_user, parent, false);
+            }
+        }
+
+        // Lookup view for data population.
+        ImageView id_image = (ImageView) view.findViewById(R.id.id_image);
+        TextView message_text = (TextView) view.findViewById(R.id.chat_message);
+        TextView timestamp = (TextView) view.findViewById(R.id.timestamp);
+
+
+        // Populate the data into the template view using the data object.
+        {
+            // Set parameters of artifact tile.
+            message_text.setText(chatMessage.getMessage_text());
+            timestamp.setText(chatMessage.getTimestamp());
+
+            //TODO: add way of adding the corresponding Artifact picture, if type==0
+
+        }
+
+        // Return the completed view to render on screen.
+        return view;
+    }
+
+    //Override this method so that we can have two custom list_item layout files
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    //Override this method so that we can have two custom list_item layout files
+    @Override
+    public int getItemViewType(int position) {
+        //get corresponding data item
+        ChatMessage chatMessage = getItem(position);
+
+        //return the type of the layout, depending on whether the message is a sent or received.
+        if(chatMessage.isFrom_user() == 0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
+    public static byte[] getBitmapAsByteArray(Bitmap bitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, outputStream);
+        return outputStream.toByteArray();
+    }
+}
